a5995c78d5a28b324aa0e08d007a490fb224956e
rachitguptaaiml <rachit.gupta_cs.aiml24@gla.ac.in>
2026-03-19 11:24:53 +0530

    Update model.py

--- ai/model.py
+++ ai/model.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
-import random
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/predict")
-def predict():
-    severity = random.choice(["Low","Medium","High"])
-    return {"severity":severity}
+class Data(BaseModel):
+    rainfall: float
+    wind: float
+
+@app.post("/predict")
+def predict(data: Data):
+
+    if data.rainfall > 50 or data.wind > 70:
+        severity = "High"
+    elif data.rainfall > 20:
+        severity = "Medium"
+    else:
+        severity = "Low"
+
+    return {"severity": severity}
