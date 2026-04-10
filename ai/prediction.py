0d6ccba6afb37758d1414a70f1ad8e8b1266f608
yashirajoriya <yashirajoriya117@gmail.com>
2026-04-10 10:54:12 +0530

    Update prediction.py

--- ai/prediction.py
+++ ai/prediction.py
@@ -1,11 +1,60 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import random
 
-def predict_disaster(rainfall, wind):
+app = FastAPI()
 
-    if rainfall > 50 or wind > 70:
-        return "High"
+class Input(BaseModel):
+    rainfall: float
+    wind: float
+    temperature: float
+    population: float
+    river_level: float
 
-    if rainfall > 20:
-        return "Medium"
+@app.post("/predict")
+def predict(data: Input):
 
-    return "Low"
+    score = 0
+
+    if data.rainfall > 200:
+        score += 30
+    elif data.rainfall > 100:
+        score += 20
+    else:
+        score += 10
+
+    if data.wind > 80:
+        score += 25
+    elif data.wind > 40:
+        score += 15
+    else:
+        score += 5
+
+    if data.temperature > 40:
+        score += 20
+    elif data.temperature > 30:
+        score += 10
+
+    if data.population > 1000:
+        score += 15
+    elif data.population > 500:
+        score += 10
+
+    if data.river_level > 8:
+        score += 25
+    elif data.river_level > 5:
+        score += 15
+
+    score += random.randint(0,10)
+
+    if score > 80:
+        severity="HIGH 🚨"
+    elif score > 50:
+        severity="MEDIUM ⚠"
+    else:
+        severity="LOW ✅"
+
+    return {
+        "severity": severity,
+        "risk_score": score
+    }
